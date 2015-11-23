fdcb770f57ad57d1428bbc788e99909cd915399c
Qbert <anonoymerengel@googlemail.com>
2015-11-23 19:07:22 +0100

    update

--- src/github.com/qbert/zbraStuff/helloworld.go
+++ src/github.com/qbert/zbraStuff/helloworld.go
@@ -2,19 +2,80 @@ package main
 
 import (
 	"net/http"
-	"os"
+	"syscall"
 	"github.com/labstack/echo"
 	mw "github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/log"
+	_ "github.com/go-sql-driver/mysql"
+    "github.com/jinzhu/gorm"
+    "github.com/qbert/zbraStuff/models"
 )
 
-// Handler
-func hello(c *echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World! I was here\n")
+var  (
+	port       string
+	ip         string
+	e          *echo.Echo
+	dbuser     string
+	dbpassword string
+	dbhost     string
+	dbport     string
+	dbname     string
+	db         *gorm.DB
+)
+
+func config_env() {
+	port = get_env("OPENSHIFT_GO_PORT", "C9_PORT", "1323")
+	ip   = get_env("OPENSHIFT_GO_IP",   "C9_IP",   "localhost")
+	
+	dbuser     = get_env("OPENSHIFT_MYSQL_DB_USERNAME", "C9_USER", "qbert")
+	dbpassword = get_env("OPENSHIFT_MYSQL_DB_PASSWORD", "C9_DB_PASSWORD", "neinegal")
+	dbhost     = get_env("OPENSHIFT_MYSQL_DB_HOST", "C9_DB_HOST", "localhost")
+	dbport     = get_env("OPENSHIFT_MYSQL_DB_PORT", "C9_DB_PORT", "3306")
+	dbname     = get_env("OPENSHIFT_MYSQL_DB_PASSWORD", "C9_DB_NAME", "zbrastuff_db")
 }
 
-func main() {
+func config_db() {
+	var dbconnect string
+	
+	dbconnect = dbuser
+	if dbpassword != "" {
+		dbconnect += ":"+dbpassword
+	}
+	dbconnect += "@"
+	
+	if dbhost != "" {
+		dbconnect += dbhost
+		
+	}
+	if dbport != "" {
+		dbconnect += ":"+dbport
+	}
+	dbconnect += "/"+dbname+"?charset=utf8&parseTime=True"
+	
+
+	log.Info("Attempting to connect to db with:%s", dbconnect)
+	
+	dbm, err := gorm.Open("mysql", dbconnect)
+    if err != nil {
+        panic("Unable to connect to the database")
+    }
+    
+    dbm.DB().Ping()
+    dbm.DB().SetMaxIdleConns(10)
+    dbm.DB().SetMaxOpenConns(100)
+    dbm.LogMode(true)
+    dbm.SingularTable(true)
+ 
+    if !dbm.HasTable(&models.User{}){
+        dbm.CreateTable(&models.User{})
+    }
+    
+    db = &dbm
+}
+
+func config_echo() {
 	// Echo instance
-	e := echo.New()
+	e = echo.New()
 
 	// Middleware
 	e.Use(mw.Logger())
@@ -22,16 +83,35 @@ func main() {
 
 	// Routes
 	e.Get("/", hello)
+}
 
-	// Start server
-	var port = os.Getenv("OPENSHIFT_GO_PORT")
-	if(port == "") {
-		port = "1323"
-	}
-	var ip = os.Getenv("OPENSHIFT_GO_IP")
-	if(ip == "") {
-		ip = "localhost"
+func run_echo() {
+	e.Run(ip+":"+port)
+}
+
+func main() {
+
+	config_env()
+	config_db()
+	config_echo()
+	
+	run_echo()
+}
+
+func get_env(e1, e2, d string) string {
+
+	if r, found := syscall.Getenv(e1); found {
+		return r
 	}
 	
-	e.Run(ip+":"+port)
+	if r, found := syscall.Getenv(e2); found {
+		return r
+	}
+	
+	return d
+}
+
+// Handler
+func hello(c *echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World! I was here\n")
 }
